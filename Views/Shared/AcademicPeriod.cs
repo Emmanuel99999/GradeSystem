453a17d463d9236612e71a9dd6df9fba9846f71e
emersonlondono <emersonlondono2005@gmail.com>
2025-09-28 00:23:09 -0500

    Create AcademicPeriod.cs

---
+++ Views/Shared/AcademicPeriod.cs
@@ -0,0 +1,12 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AcademicGradingSystem.Shared.Models
+{
+    public class AcademicPeriod
+    {
+        public int id { get; set; }
+        public string? nombre { get; set; }
+
+    }
+}
